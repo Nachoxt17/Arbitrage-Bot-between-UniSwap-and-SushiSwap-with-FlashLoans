e6180c03ffd2fd38f2c810b3380e3254b05e480f
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-05-19 00:16:27 -0300

    Added Tests

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
-  string public name = "EthSwap Instant Exchange";
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        //+-Redemption rate = Number of Tokens they receive for 1 Ether:_
+        //+-Calculate the number of Tokens to buy:_
+        uint256 tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
 }
