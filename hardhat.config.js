8dd71441bc1ee3013e39b6fdf136b94033933032
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-05-20 21:20:12 -0300

    feat: Added UniSwapV2Router Swap

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,7 +45,7 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.6.2",
   paths: {
     artifacts: "./src/artifacts",
   },
